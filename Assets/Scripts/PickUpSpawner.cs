db820adf3b1403711d4a0373a5e542dfa10aa94c
DukeOfDill <48437657+DukeOfDill@users.noreply.github.com>
2019-03-11 17:08:30 +0100

    Made a PickUp Spawner

---
+++ Assets/Scripts/PickUpSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpSpawner : MonoBehaviour
+{
+    public GameObject refobject;
+    public List<GameObject> allObjects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnPickUp();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void SpawnPickUp()
+    {
+        //Instantiate(refobject, new Vector2(0,0), Quaternion.identity);
+        allObjects.Add(Instantiate(refobject, new Vector2(0, 0), Quaternion.identity));
+    }
+    public void Reset()
+    {
+        for(int i = 0; i < allObjects.Count; i++)
+        {
+            if (allObjects[i] != null)
+            {
+                Destroy(allObjects[i]);
+            }           
+        }
+        CleanList();
+    }
+    void CleanList()
+    {
+        for( int i = allObjects.Count; i > -1; i--)
+        {
+            if (allObjects[i] == null)
+            {
+                allObjects.RemoveAt(i);
+            }
+        }
+    }
+}
