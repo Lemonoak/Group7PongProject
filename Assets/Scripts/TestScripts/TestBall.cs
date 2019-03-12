997110b6a4ae793cbbca009dc8825188ba42a00a
Lemonoak <45938598+Lemonoak@users.noreply.github.com>
2019-03-12 11:54:06 +0100

    Added a testball that moves side to side
    Added a testball that moves side to side at desired speed (public variable that everyone can change)

---
+++ Assets/Scripts/TestScripts/TestBall.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBall : MonoBehaviour
+{
+    public Rigidbody2D RB;
+
+    public float Speed = 1f;
+
+    public bool IsMovingRight = false;
+
+    void Start()
+    {
+        RB = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if(RB.transform.position.x > 15)
+        {
+            IsMovingRight = false;
+        }
+        if (RB.transform.position.x < -15)
+        {
+            IsMovingRight = true;
+        }
+
+        if(IsMovingRight)
+        {
+            transform.Translate(new Vector3(1.0f * Speed, 0.0f, 0.0f));
+        }
+        if (!IsMovingRight)
+        {
+            transform.Translate(new Vector3(-1.0f * Speed, 0.0f, 0.0f));
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "PickUp")
+        {
+            Debug.Log("You tried to pick something up");
+        }
+    }
+}
