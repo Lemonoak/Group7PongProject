aa6aa1cab15f07eb92d4a342c8f28b45b00e72ce
LaQtoz <48442561+LaQtoz@users.noreply.github.com>
2019-03-13 14:22:02 +0100

    goal code

--- Assets/Goal.cs
+++ Assets/Goal.cs
@@ -7,12 +7,28 @@ public class Goal : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ball")
+        {
+            Debug.Log("Score");
+        }
+
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        }
+    }
+
+
+
