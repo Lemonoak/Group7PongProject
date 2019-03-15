687d1279157d0428c0c3f839d07e069e8e238c38
LaQtoz <48442561+LaQtoz@users.noreply.github.com>
2019-03-15 14:40:13 +0100

    fixed a simple pause/start menu
    have to fix it so it exists on both screens

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject PauseMenuUi;
+
+    [SerializeField] private bool isPaused;
+
+
+    private void Update()
+    {
+        
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+
+        if (isPaused)
+        {
+            ActivateMenu();
+
+        }
+
+        else
+        {
+            DeactivateMenu();
+        }
+    }
+
+    void ActivateMenu()
+    {
+        Time.timeScale = 0;
+        AudioListener.pause = true;
+        PauseMenuUi.SetActive(true);
+
+    }
+
+   public void DeactivateMenu()
+    {
+        Time.timeScale = 1;
+        AudioListener.pause = false;
+        PauseMenuUi.SetActive(false);
+        isPaused = false;
+    }
+
+}
