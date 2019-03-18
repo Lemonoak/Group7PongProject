dfeef9e982f087129bc78393e391dab3eac6993d
Lemonoak <45938598+Lemonoak@users.noreply.github.com>
2019-03-18 14:40:47 +0100

    Imported TextMeshPro and added font
    Added pixel font as a textmeshpro font
    Added text mesh pro to the project

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -18,17 +18,36 @@ public class AI : MonoBehaviour
     void HandleMovement()
     {
         //Checks if there is a ball and then if the balls position is on the ai's screen
-        if(Ball && Ball.transform.position.x > 0)
+        if(transform.position.x > 0)
         {
-            //Moves the ai upwards
-            if(Ball.transform.position.y > gameObject.transform.position.y)
+            if(Ball && Ball.transform.position.x > 0)
             {
-                transform.Translate(new Vector3(Time.deltaTime * AISpeed, 0.0f, 0.0f));
+                //Moves the ai upwards
+                if(Ball.transform.position.y > gameObject.transform.position.y)
+                {
+                    transform.Translate(new Vector3(Time.deltaTime * AISpeed, 0.0f, 0.0f));
+                }
+                //Moves the ai downwards
+                else if (Ball.transform.position.y < gameObject.transform.position.y)
+                {
+                    transform.Translate(new Vector3(-Time.deltaTime * AISpeed, 0.0f, 0.0f));
+                }
             }
-            //Moves the ai downwards
-            else if (Ball.transform.position.y < gameObject.transform.position.y)
+        }
+        else if (transform.position.x < 0)
+        {
+            if (Ball && Ball.transform.position.x < 0)
             {
-                transform.Translate(new Vector3(-Time.deltaTime * AISpeed, 0.0f, 0.0f));
+                //Moves the ai upwards
+                if (Ball.transform.position.y > gameObject.transform.position.y)
+                {
+                    transform.Translate(new Vector3(Time.deltaTime * AISpeed, 0.0f, 0.0f));
+                }
+                //Moves the ai downwards
+                else if (Ball.transform.position.y < gameObject.transform.position.y)
+                {
+                    transform.Translate(new Vector3(-Time.deltaTime * AISpeed, 0.0f, 0.0f));
+                }
             }
         }
     }
